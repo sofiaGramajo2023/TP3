e2619e23fb235fab23aeb8229655d3279ba85a9f
Sofia <sofia.asgp@gmail.com>
2023-03-15 01:28:54 -0300

    Resolución ejericio 7 (Último)

--- js/ej7.js
+++ js/ej7.js
@@ -2,6 +2,23 @@
  la creación de la tabla debe ser realizada con una función y mostrar solo los resultados 
  del 1 al 10 del número elegido por el usuario */
 
- 
 
 
+
+function escribirTablaMultiplicar(numero){
+
+	document.write("<h2>Tabla de multiplicar del "+ numero +"</h2>");
+
+	document.write("<ul>");
+	
+
+	for(i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(numero + "x " + i + "= " + numero * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
+}
+
+escribirTablaMultiplicar(prompt('Ingrese un número'));
